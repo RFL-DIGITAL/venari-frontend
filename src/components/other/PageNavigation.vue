cfb6709bcc766e0b68308a99177d2ba2c1455559
Маняков Степан <stepanmanyakov@gmail.com>
2024-06-08 16:35:53 +0700

    feat: Разметка страниц. Поправил шрифт. Навигация

---
+++ src/components/other/PageNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="to">
+    <div class="text-page-title col-span-2 flex gap-x-[8px] flex items-center">
+      <p>{{ label }}</p>
+      <i class="w-[18px] h-[28px] text-black icon-[outlined/arrow-right]" />
+    </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+  import { RouteLocationRaw } from 'vue-router'
+
+  interface Props {
+    label: string
+    to: RouteLocationRaw
+  }
+
+  defineProps<Props>()
+</script>
+
+<style scoped></style>
---
+++ src/components/other/PageTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-page-title col-span-2">
+    {{ title }}
+    <p class="text-sm text-gray" v-if="description">
+      Найдите чат с единомышленниками и заведите полезные знакомства
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    title: string
+    description: string
+  }
+
+  defineProps<Props>()
+</script>
+
+<style scoped></style>
